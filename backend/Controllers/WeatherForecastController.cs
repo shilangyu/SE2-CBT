9296404442e187bf383e0a892d6412478b845923
shilangyu <xmarcinmarcin@gmail.com>
2022-03-09 21:34:53 +0100

    Disable csharp_new_line_before_open_brace

--- backend/Controllers/WeatherForecastController.cs
+++ backend/Controllers/WeatherForecastController.cs
@@ -4,8 +4,7 @@ namespace CbtBackend.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
-{
+public class WeatherForecastController : ControllerBase {
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing" , "Chilly"  , "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -13,18 +12,15 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
+    public WeatherForecastController(ILogger<WeatherForecastController> logger) {
         this.logger = logger;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
+    public IEnumerable<WeatherForecast> Get() {
         logger.LogDebug("Sending weather");
 
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast {
             Date = DateTime.Now.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
