63378cac57a72c22ca1a17a689a72b4096420806
Kamil Monicz <kamil@monicz.pl>
2022-03-16 23:30:19 +0100

    Dockerize backend, Closes #5
    HOW-TO:
    docker build -t se2-back .
    docker run -it --rm se2-back

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,6 @@
+using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.DataProtection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,10 +10,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Explicitly set data protection keys directory to make it Docker-volume compatible
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+{
+    builder.Services.AddDataProtection().PersistKeysToFileSystem(new DirectoryInfo("keys"));
+}
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
 }
