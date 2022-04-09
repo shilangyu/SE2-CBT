0c624b06e3edcb6b3542cfa16cedad5812b7519e
wolftender <etheryt@protonmail.com>
2022-04-09 12:41:51 +0200

    move initialization to startup class

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,81 +1,14 @@
-using System.Text;
-using System.Runtime.InteropServices;
-using CbtBackend;
-using Microsoft.AspNetCore.DataProtection;
-using Microsoft.IdentityModel.Tokens;
+namespace CbtBackend;
 
-using CbtBackend.Services;
-using Microsoft.AspNetCore.HttpOverrides;
+public class Program {
+    public static void Main(string[] args) {
+        var host = CreateHostBuilder(args).Build();
+        host.Run();
+    }
 
-var builder = WebApplication.CreateBuilder(args);
-
-if (builder.Environment.IsDevelopment()) {
-    builder.Services.AddCors(options => {
-        options.AddDefaultPolicy(builder => {
-            builder.SetIsOriginAllowed(origin => new Uri(origin).IsLoopback)
-                .AllowAnyMethod()
-                .AllowAnyHeader();
+    public static IHostBuilder CreateHostBuilder(string[] args) {
+        return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => {
+            webBuilder.UseStartup<Startup>();
         });
-    });
-}
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// register services for DI
-{
-    builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
-    builder.Services.AddScoped<IUserService, UserService>();
-    builder.Services.AddDbContext<CbtDbContext>();
-}
-
-// authentication
-{
-    builder.Services.AddAuthentication("jwtauth").AddJwtBearer("jwtauth", options => {
-        options.TokenValidationParameters = new TokenValidationParameters() {
-            ValidateIssuerSigningKey = true,
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]))
-        };
-    });
-}
-
-// Explicitly set data protection keys directory to make it Docker-volume compatible
-if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
-    builder.Services.AddDataProtection().PersistKeysToFileSystem(new DirectoryInfo("keys"));
-}
-
-// Forwarded Headers Middleware
-// https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/proxy-load-balancer?view=aspnetcore-6.0
-builder.Services.Configure<ForwardedHeadersOptions>(options => {
-    options.ForwardedHeaders = ForwardedHeaders.All;
-});
-
-var app = builder.Build(); {
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment()) {
-        app.UseSwagger();
-        app.UseSwaggerUI();
     }
-
-    // Current deploy uses an HTTPS proxy with redirection enabled
-    // It also passes X-Forwarded-* headers with true client IPs
-    if (Utilities.IsDocker()) {
-        app.UseForwardedHeaders();
-    } else {
-        app.UseHttpsRedirection();
-    }
-
-    app.UseCors();
-    app.UseAuthentication();
-    app.UseAuthorization();
-    app.MapControllers();
-    app.Run();
 }
