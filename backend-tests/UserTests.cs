e487028e2ab10bf6d1238d1d6e3e444bfc0632b8
wolftender <etheryt@protonmail.com>
2022-04-13 12:45:14 +0200

    change user endpoint tests

--- backend-tests/UserTests.cs
+++ backend-tests/UserTests.cs
@@ -1,10 +1,13 @@
 namespace CbtBackend.Tests;
 
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-
+using CbtBackend.Entities;
+using CbtBackend.Services;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 public class UserTests : IClassFixture<CustomWebApplicationFactory<Startup>> {
@@ -17,11 +20,37 @@ public class UserTests : IClassFixture<CustomWebApplicationFactory<Startup>> {
     [Theory]
     [InlineData("/" + Contracts.ApiRoutes.User.GetAll)]
     [InlineData("/" + Contracts.ApiRoutes.User.GetByEmail)]
-    [InlineData("/" + Contracts.ApiRoutes.User.DeleteByEmail)]
-    [InlineData("/" + Contracts.ApiRoutes.User.UpdateByEmail)]
     public async Task EndpointIsProtectedFromAnonymous(string endpoint) {
         var client = factory.CreateClient();
         var res = await client.GetAsync(endpoint);
+
+        Assert.Equal(HttpStatusCode.Unauthorized, res.StatusCode);
+    }
+
+    [Theory]
+    [InlineData("/" + Contracts.ApiRoutes.User.GetAll)]
+    [InlineData("/" + Contracts.ApiRoutes.User.GetByEmail)]
+    public async Task EndpointProtectedFromMissingPermissions(string endpoint) {
+        var client = factory.CreateClient();
+
+        HttpResponseMessage res;
+        string token;
+
+        using (var scope = factory.Services.CreateScope()) {
+            var jwtTokenService = scope.ServiceProvider.GetRequiredService<IJwtTokenService>();
+            var user = new User();
+
+            user.Email = "xd@xd.pl";
+            user.Password = "9494949494d949d4w9d9w4d9w";
+            user.Roles = new List<string>();
+
+            token = jwtTokenService.GenerateToken(user, System.DateTime.Now);
+        }
+
+        using (var reqMessage = new HttpRequestMessage(HttpMethod.Get, endpoint)) {
+            reqMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            res = await client.SendAsync(reqMessage);
+        }
 
         Assert.Equal(HttpStatusCode.Forbidden, res.StatusCode);
     }
