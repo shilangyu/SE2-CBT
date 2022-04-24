2de70dbe4ea8f0d8d30c445ea962bbccde6cabf3
shilangyu <xmarcinmarcin@gmail.com>
2022-04-24 13:16:10 +0200

    Upgrade to react 18

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,17 +1,18 @@
 import { ThemeProvider } from '@emotion/react'
 import { SnackbarProvider } from 'notistack'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import { theme } from './theme'
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')!)
+
+root.render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
             <SnackbarProvider maxSnack={3}>
                 <App />
             </SnackbarProvider>
         </ThemeProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 )
