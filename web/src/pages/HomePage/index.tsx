15d12aef8992269c4ab688b1cc0a74179d29f3a9
shilangyu <xmarcinmarcin@gmail.com>
2022-04-24 14:15:47 +0200

    Add home page

--- web/src/pages/HomePage/index.tsx
+++ web/src/pages/HomePage/index.tsx
@@ -1,7 +1,25 @@
-import { Button, Grid, TextField } from '@mui/material'
+import {
+    AppBar,
+    Button,
+    Grid,
+    IconButton,
+    Stack,
+    TextField,
+    Toolbar,
+    Typography,
+    styled,
+} from '@mui/material'
 import { useSnackbar } from 'notistack'
 import * as React from 'react'
 import { useLoginStore } from '../../stores/loginStore'
+import LogoutIcon from '@mui/icons-material/Logout'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import { HashRouter, Link, Navigate, Route, Routes } from 'react-router-dom'
+import ProfilePage from '../ProfilePage'
+import HomeIcon from '@mui/icons-material/Home'
+import { routes } from '../routes'
+
+const ToolbarOffset = styled('div')(({ theme }) => theme.mixins.toolbar)
 
 function HomePage() {
     const logOut = useLoginStore(e => e.logOut)
@@ -13,19 +31,68 @@ function HomePage() {
     }
 
     return (
-        <Grid
-            container
-            direction="column"
-            rowSpacing={4}
-            alignItems="center"
-            justifyContent="center"
-            style={{ minHeight: '100vh' }}
-        >
-            <Grid item>Logged in!</Grid>
-            <Button variant="outlined" onClick={onLogout}>
-                Log out
-            </Button>
-        </Grid>
+        <>
+            <AppBar position="fixed">
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        Cognitive behavioral therapy
+                    </Typography>
+                    <Stack direction="row" alignItems="center" spacing={1}>
+                        <Routes>
+                            <Route
+                                index
+                                element={
+                                    <IconButton
+                                        size="large"
+                                        component={Link}
+                                        to={routes.profile()}
+                                    >
+                                        <AccountCircleIcon fontSize="inherit" />
+                                    </IconButton>
+                                }
+                            />
+                            <Route
+                                path={routes.profile.pattern}
+                                element={
+                                    <IconButton
+                                        size="large"
+                                        component={Link}
+                                        to={routes.home()}
+                                    >
+                                        <HomeIcon fontSize="inherit" />
+                                    </IconButton>
+                                }
+                            />
+                        </Routes>
+
+                        <Button
+                            color="inherit"
+                            variant="text"
+                            onClick={onLogout}
+                            startIcon={<LogoutIcon />}
+                        >
+                            Log out
+                        </Button>
+                    </Stack>
+                </Toolbar>
+            </AppBar>
+            <ToolbarOffset />
+            <Routes>
+                <Route index element={<div>here</div>} />
+                <Route
+                    path={routes.profile.pattern}
+                    element={<ProfilePage />}
+                />
+                <Route
+                    path="*"
+                    element={<Navigate to={routes.home()} replace />}
+                />
+            </Routes>
+        </>
     )
 }
 
