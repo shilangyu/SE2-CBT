e0ade171c98e095ba1d3dd6a2c2283dfe16d8a72
wolftender <etheryt@protonmail.com>
2022-04-26 12:55:24 +0200

    add basic user page

--- web/src/pages/ProfilePage/index.tsx
+++ web/src/pages/ProfilePage/index.tsx
@@ -7,17 +7,58 @@ import {
     TextField,
     Toolbar,
     Typography,
+    useTheme,
 } from '@mui/material'
 import { useSnackbar } from 'notistack'
 import * as React from 'react'
 import { useLoginStore } from '../../stores/loginStore'
 import LogoutIcon from '@mui/icons-material/Logout'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import { parseJwt } from '../../utils/jwt'
+import { apiClient } from '../../api'
+import { User } from '../../model/user'
 
 function ProfilePage() {
+    const theme = useTheme();
+    const loginStore = useLoginStore();
+
+    const [userLogin, setUserLogin] = React.useState<string>('...');
+    const [userData, setUserData] = React.useState<User | undefined>(undefined);
+
+    const { enqueueSnackbar } = useSnackbar()
+    
+    React.useEffect(() => {
+        // assume token is not null
+        const tokenClaims = parseJwt(loginStore.token as string);
+        const tokenUserLogin = tokenClaims.unique_name;
+
+        setUserLogin(tokenUserLogin)
+        setUserData(undefined)
+
+        apiClient.getUser(tokenUserLogin).then(currentUser => {
+            setUserData(currentUser)
+        }).catch(error => {
+            console.error(`failed to fetch user data: ${error}`);
+            enqueueSnackbar('failed to fetch user data (details in console)', { variant: 'error' })
+        });
+    }, [])
+
+    const renderUserProfile = () => {
+        if (userData) {
+            return <Typography>{userData.gender}</Typography>
+        } else {
+            return <Typography>loading...</Typography>
+        }
+    }
+
     return (
         <>
-            <Typography>User profile</Typography>
+            <Grid container>
+                <Grid item>
+                    <Typography>{userLogin}</Typography>
+                    {renderUserProfile()}
+                </Grid>
+            </Grid>
         </>
     )
 }
---
+++ web/src/utils/jwt.ts
@@ -0,0 +1,17 @@
+import {UserTokenClaims} from '../model/user'
+import {Buffer} from 'buffer'
+
+export class JwtParseError extends Error { }
+
+export const parseJwt = (token : string) : UserTokenClaims => {
+    const tokenParts = token.split('.');
+
+    if (tokenParts.length !== 3) {
+        throw new JwtParseError('invalid jwt format');
+    }
+
+    const claimsBuffer = Buffer.from(tokenParts[1], 'base64');
+    const claimsString = claimsBuffer.toString('utf8');
+    
+    return JSON.parse(claimsString) as UserTokenClaims;
+}
