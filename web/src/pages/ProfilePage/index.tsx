7db8cd94b5ea8dc7bfdf5866b13b9228fe76347e
wolftender <etheryt@protonmail.com>
2022-04-26 15:06:43 +0200

    add user info display

--- web/src/pages/ProfilePage/index.tsx
+++ web/src/pages/ProfilePage/index.tsx
@@ -8,6 +8,11 @@ import {
     Toolbar,
     Typography,
     useTheme,
+    Card,
+    Container,
+    Icon,
+    CircularProgress,
+    Divider,
 } from '@mui/material'
 import { useSnackbar } from 'notistack'
 import * as React from 'react'
@@ -17,6 +22,8 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import { parseJwt } from '../../utils/jwt'
 import { apiClient } from '../../api'
 import { User } from '../../model/user'
+import { maxWidth, padding } from '@mui/system'
+import PersonIcon from '@mui/icons-material/Person'
 
 function ProfilePage() {
     const theme = useTheme()
@@ -24,6 +31,7 @@ function ProfilePage() {
 
     const [userLogin, setUserLogin] = React.useState<string>('...')
     const [userData, setUserData] = React.useState<User | undefined>(undefined)
+    const [editMode, setEditMode] = React.useState(false)
 
     const { enqueueSnackbar } = useSnackbar()
 
@@ -49,22 +57,178 @@ function ProfilePage() {
             })
     }, [])
 
+    const renderUserEditor = () => {
+        return (
+            <>
+                <Grid item xs={2}>
+                    <Typography sx={{ fontWeight: 'bold' }}>login</Typography>
+                </Grid>
+                <Grid item xs={10}>
+                    <Typography>{userData?.login}</Typography>
+                </Grid>
+                <Grid item xs={2}>
+                    <Typography sx={{ fontWeight: 'bold' }}>
+                        password
+                    </Typography>
+                </Grid>
+                <Grid item xs={10}>
+                    <Typography>
+                        protected and definitely not exposed through api :)
+                    </Typography>
+                </Grid>
+                <Grid item xs={2}>
+                    <Typography sx={{ fontWeight: 'bold' }}>age</Typography>
+                </Grid>
+                <Grid item xs={10}>
+                    <Typography>{userData?.age}</Typography>
+                </Grid>
+                <Grid item xs={2}>
+                    <Typography sx={{ fontWeight: 'bold' }}>gender</Typography>
+                </Grid>
+                <Grid item xs={10}>
+                    <Typography>{userData?.gender}</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Stack direction="row" spacing={2}>
+                        <Button variant="contained" disableElevation>
+                            save
+                        </Button>
+                        <Button
+                            variant="outlined"
+                            disableElevation
+                            onClick={() => setEditMode(false)}
+                        >
+                            cancel
+                        </Button>
+                    </Stack>
+                </Grid>
+            </>
+        )
+    }
+
+    const renderUserInfo = () => {
+        return (
+            <>
+                <Grid item xs={2}>
+                    <Typography sx={{ fontWeight: 'bold' }}>login</Typography>
+                </Grid>
+                <Grid item xs={10}>
+                    <Typography>{userData?.login}</Typography>
+                </Grid>
+                <Grid item xs={2}>
+                    <Typography sx={{ fontWeight: 'bold' }}>
+                        password
+                    </Typography>
+                </Grid>
+                <Grid item xs={10}>
+                    <Typography>
+                        protected and definitely not exposed through api :)
+                    </Typography>
+                </Grid>
+                <Grid item xs={2}>
+                    <Typography sx={{ fontWeight: 'bold' }}>age</Typography>
+                </Grid>
+                <Grid item xs={10}>
+                    <Typography>{userData?.age}</Typography>
+                </Grid>
+                <Grid item xs={2}>
+                    <Typography sx={{ fontWeight: 'bold' }}>gender</Typography>
+                </Grid>
+                <Grid item xs={10}>
+                    <Typography>{userData?.gender}</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button
+                        variant="contained"
+                        disableElevation
+                        onClick={() => setEditMode(true)}
+                    >
+                        edit
+                    </Button>
+                </Grid>
+            </>
+        )
+    }
+
     const renderUserProfile = () => {
         if (userData) {
-            return <Typography>{userData.gender}</Typography>
+            return (
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Typography variant="h5">information</Typography>
+                        <Typography color={theme.palette.text.secondary}>
+                            here you can edit info about your account
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Divider />
+                    </Grid>
+                    <>
+                        {(() =>
+                            editMode ? renderUserEditor() : renderUserInfo())()}
+                    </>
+                </Grid>
+            )
         } else {
-            return <Typography>loading...</Typography>
+            return (
+                <Container
+                    sx={{ justifyContent: 'center', textAlign: 'center' }}
+                >
+                    <CircularProgress />
+                </Container>
+            )
         }
     }
 
     return (
         <>
-            <Grid container>
-                <Grid item>
-                    <Typography>{userLogin}</Typography>
-                    {renderUserProfile()}
+            <Container maxWidth="xl">
+                <Grid
+                    container
+                    spacing={2}
+                    sx={{ width: '100%', display: 'block' }}
+                >
+                    <Grid item xs={12}>
+                        <Card variant="outlined" elevation={0}>
+                            <Grid container>
+                                <Grid
+                                    item
+                                    display={{ md: 'block', xs: 'none' }}
+                                    md={2}
+                                    lg={1}
+                                >
+                                    <PersonIcon
+                                        style={{
+                                            width: '100%',
+                                            height: '100%',
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid item sm={8} sx={{ padding: 3 }}>
+                                    <Typography variant="h3">
+                                        {userLogin}
+                                    </Typography>
+                                    <Typography
+                                        variant="h5"
+                                        color={theme.palette.text.secondary}
+                                    >
+                                        user profile page
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Card
+                            variant="outlined"
+                            elevation={0}
+                            sx={{ padding: 3 }}
+                        >
+                            {renderUserProfile()}
+                        </Card>
+                    </Grid>
                 </Grid>
-            </Grid>
+            </Container>
         </>
     )
 }
--- web/src/theme.ts
+++ web/src/theme.ts
@@ -1,10 +1,12 @@
 import { createTheme } from '@mui/material'
-import { orange } from '@mui/material/colors'
+import { orange, grey } from '@mui/material/colors'
 
 export const theme = createTheme({
     palette: {
         primary: { main: orange[500] },
+        text: { primary: grey[900], secondary: grey[500] },
     },
+
     components: {
         MuiTextField: {
             defaultProps: {
