8bdf5320688fb286f2fd9635bc56d42e417da776
shilangyu <xmarcinmarcin@gmail.com>
2022-05-11 10:19:30 +0200

    Initialize dbsets

--- backend/CbtBackend/Services/CbtDbContext.cs
+++ backend/CbtBackend/Services/CbtDbContext.cs
@@ -1,13 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using CbtBackend.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 
 namespace CbtBackend.Services;
 
 public sealed class CbtDbContext : IdentityDbContext<User, IdentityRole, string> {
-    public DbSet<MudTest> Evaluations;
-    public DbSet<MudTestResponse> EvaluationResponses;
+    public DbSet<MudTest> Evaluations => Set<MudTest>();
+    public DbSet<MudTestResponse> EvaluationResponses => Set<MudTestResponse>();
 
     public CbtDbContext(DbContextOptions<CbtDbContext> options) : base(options) {
     }
